b70f69c3facf5d1b92329dcf850b789127af02c9
tomerle03 <70287120+tomerle03@users.noreply.github.com>
2022-09-12 16:54:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+names = "tomer leibovitch\nliad mizrahi\nitay shitrit\nron limor\nshaked stern"
+stam = "yesssss"
+
 @app.route("/")
 def hello_world():
-    return "Hello world"
+    return names
+
+@app.route("/ron")
+def ron():
+    return stam
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
-    
